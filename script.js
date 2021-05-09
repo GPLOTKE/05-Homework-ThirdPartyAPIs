ce9d940aa3bdc2d6c2e1484f6324e95cdd90406c
Jerry Plotke <jerry.plotke@gmail.com>
2021-05-09 00:29:03 -0500

    attempting function to save event

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 var currentDayEl = $('#currentDay');
 
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    currentDayEl.text(rightNow);
+    var rightNowEl = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    currentDayEl.text(rightNowEl);
 }
 
 setInterval(displayTime, 1000);
+
+
+// need to change the timeblock area color based on past, present, or future time
+
+// need current time variable
+// need variable
+// for rows classes
+// for loop through rows classes
+// need i ID as a string
+// get el by ID
+// if/else statements for past / present / future
+
+// var currentHourEl = rightNowEl.format('HH');
+
+
+// var textInputEl = $(".col");
+// var saveBtnEl = ('.saveBtn');
+
+
+
+// saveBtnEl.addEventListener("click", function(event) {
+//             event.preventDefault();
